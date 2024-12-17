8b07f0f4a1a6180c43c12ee1c9b0c75ee80b5bc4
sukanya gurav <sukanyagurav6@gmail.com>
2024-12-17 23:36:21 +0530

    updated css for modal

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,12 +1,36 @@
-import React from 'react'
+import React from "react";
 
-const Input = ({labelText, type, id,placeholder=""}) => {
+const Input = ({
+  labelText,
+  type,
+  id,
+  errors,
+  register,
+  name,
+  valueAsNumber,
+  placeholder = "",
+}) => {
+  console.log(register);
   return (
-    <div className='flex flex-col'>
-      <label htmlFor={id} className='opacity-[0.8] mt-4 flex-1'>{labelText}</label>
-      <input type={type} id={id} placeholder={placeholder} className='w-full border-[0.1rem] my-2 p-2 rounded-lg bg-transparent focus:outline-[#7C5DFA] flex-1 outline-none focus:border-none '  />
-    </div>
-  )
-}
+    <div className="flex flex-col mb-2 relative">
+      <label htmlFor={id} className="opacity-[0.8] mt-4 flex-1">
+        {labelText}
+      </label>
+      <input
+        type={type}
+        id={id}
+        placeholder={placeholder}
+        className={`w-full border-[0.1rem] my-2 p-2 rounded-lg bg-transparent focus:outline-[#7C5DFA] flex-1 outline-none focus:border-none  ${
+          errors ? "border-red" : "border-slate_500"
+        }`}
+        {...register(name)}
+      />
 
-export default Input
+      <span className="text-red-500 text-[0.75rem] block absolute -bottom-4">
+        {errors ? errors.message : ""}
+      </span>
+    </div>
+  );
+};
+
+export default Input;
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,8 +1,42 @@
 import React, { useState } from "react";
 import Input from "./Input";
 import { v4 as uuidv4 } from "uuid";
+import { useForm } from "react-hook-form";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+const items = {
+  itemName: z.string().min(2, { message: "Please enter item name" }),
+  quantity: z.number({ message: "This field is required!" }),
+  price: z.number({ message: "This field is required!" }),
+};
+const schemaTypes = {
+  fromStreetAddress: z
+    .string()
+    .min(15, { message: "Please enter Street address" }),
+  // fromCity:z.string().min(3,{ message:'Please enter City'}),
+  // fromPostalCode:  z.number({message:'This field is required!'}),
+  // fromCountry:z.string().min(3,{ message:'Please enter Country'}),
+  // clientName:z.string().min(2,{ message:'Please enter Client Name'}),
+  // clientEmail:z.string().email({ message:'Please enter a valid email'}),
+  // clientStreetAddress:z.string().min(15,{ message:'Please enter Client Street address'}),
+  // clientCity:z.string().min(3,{ message:'Please enter Client City'}),
+  // clientPostalCode: z.number({message:'This field is required!'}),
+  // clientCountry:z.string().min(3,{ message:'Please enter client Country'}),
+  // description:z.string().min(2,{ message:'Please enter description'}),
+  // items:z.array(items)
+};
 const Modal = ({ closeModal, children, isOpen }) => {
   const [itemList, setItemList] = useState([]);
+  const schema = z.object(schemaTypes);
+  const {
+    register,
+    formState: { errors, isSubmitSuccessful },
+    handleSubmit,
+    reset,
+  } = useForm({ resolver: zodResolver(schema) });
+
   function addItem() {
     setItemList([
       ...itemList,
@@ -13,6 +47,10 @@ const Modal = ({ closeModal, children, isOpen }) => {
   function removeItem(index) {
     const newArray = [...itemList].filter((item) => item.id !== index);
     setItemList(newArray);
+  }
+
+  function validateData(data){
+    console.log(data)
   }
   return (
     <>
@@ -25,134 +63,213 @@ const Modal = ({ closeModal, children, isOpen }) => {
         open={isOpen}
       >
         <h2 className="text-3xl font-bold">Create Invoice</h2>
-        <div className="my-8 mb-0 overflow-y-scroll max-h-[480px] h-full pr-8 pl-1">
-          <h3 className="text-[#7C5DFA] font-bold">Bill From</h3>
-          <div>
-            <Input
-              id="fromStreetAddress"
-              labelText="Street Address"
-              type="text"
-            />
-
-            <div className="flex gap-4">
-              <Input id="fromCity" labelText="City" type="text" />
-
+        <form onSubmit={handleSubmit(validateData)} noValidate="noValidate">
+          <div className="my-8 mb-0 overflow-y-scroll max-h-[480px] h-full pr-8 pl-1">
+            <h3 className="text-[#7C5DFA] font-bold">Bill From</h3>
+            <div>
               <Input
-                id="fromPostalCode"
-                labelText="Postal Code"
-                type="number"
+                id="fromStreetAddress"
+                labelText="Street Address"
+                type="text"
+                name="fromStreetAddress"
+                register={register}
+                errors={errors.fromStreetAddress}
               />
 
-              <Input id="fromCountry" labelText="Country" type="text" />
-            </div>
-          </div>
-          <h3 className="text-[#7C5DFA] mt-6 font-bold">Bill To</h3>
-          <div>
-            <Input
-              id="billToClientName"
-              labelText="Client's Name"
-              type="text"
-            />
+              <div className="flex gap-4">
+                <Input
+                  id="fromCity"
+                  labelText="City"
+                  type="text"
+                  name="fromStreetAddress"
+                  register={register}
+                  errors={errors.fromStreetAddress}
+                />
 
-            <Input
-              id="billToStreetAddress"
-              labelText="Client's Email"
-              type="email"
-              placeholder="e.g. email@example.com"
-            />
+                <Input
+                  id="fromPostalCode"
+                  labelText="Postal Code"
+                  type="number"
+                  name="fromStreetAddress"
+                  register={register}
+                  errors={errors.fromStreetAddress}
+                />
 
-            <Input
-              id="billToStreetAddress"
-              labelText="Street Address"
-              type="text"
-            />
-            <div className="flex gap-4">
-              <Input id="billToCity" labelText="City" type="text" />
-              <Input
-                id="billToPostalCode"
-                labelText="Postal Code"
-                type="number"
-              />
-              <Input id="billToCountry" labelText="Country" type="text" />
-            </div>
-            <div className="flex gap-4 ">
-              <Input
-                type="date"
-                labelText="Invoice Date"
-                id="billToInvoiceDate"
-              />
-
-              <div className="flex-1 flex flex-col  relative">
-                <label
-                  htmlFor="payementTerms"
-                  className="opacity-[0.8] mt-4 mb-2 "
-                >
-                  Payment Terms
-                </label>
-                <select
-                  id="payementTerms"
-                  className="p-[0.6rem] border-[0.1rem]  rounded-lg bg-transparent focus:outline-[#7C5DFA] appearance-none"
-                >
-                  <option value="1">Net 1 Day</option>
-                  <option value="7">Net 7 Days</option>
-                  <option value="14">Net 14 Days</option>
-                  <option value="30">Net 30 Days</option>
-                </select>
-                <span
-                  className={`fa-solid fa-angle-down transition-all duration-300 absolute top-[65%] right-6`}
-                ></span>
+                <Input
+                  id="fromCountry"
+                  labelText="Country"
+                  type="text"
+                  name="fromStreetAddress"
+                  register={register}
+                  errors={errors.fromStreetAddress}
+                />
               </div>
             </div>
-            <Input type="text" labelText="Description" id="description" />
+            <h3 className="text-[#7C5DFA] mt-6 font-bold">Bill To</h3>
+            <div>
+              <Input
+                id="billToClientName"
+                labelText="Client's Name"
+                type="text"
+                name="fromStreetAddress"
+                register={register}
+                errors={errors.fromStreetAddress}
+              />
 
-            <h3 className="font-bold my-8  opacity-[0.7]">ItemList</h3>
-            <div className="flex flex-col itemList ">
-              {itemList.map((item, index) => (
-                <div className="flex gap-4 " key={item.id}>
-                  <Input type="text" labelText="Item Name" id="itemName" />
-                  <Input type="number" labelText="Qty." id="quantity" />
-                  <Input type="number" labelText="Price" id="price" />
-                  <div className="flex flex-col ">
-                    <label htmlFor="total" className="opacity-[0.8] mt-4  ">
-                      Total
-                    </label>
-                    <span className=" p-2 mt-3 block w-10">{item.total}</span>
-                  </div>
-                  <button
-                    className="my-4 block hover:text-red-400 self-center"
-                    onClick={() => removeItem(item.id)}
+              <Input
+                id="billToStreetAddress"
+                labelText="Client's Email"
+                type="email"
+                placeholder="e.g. email@example.com"
+                name="fromStreetAddress"
+                register={register}
+                errors={errors.fromStreetAddress}
+              />
+
+              <Input
+                id="billToStreetAddress"
+                labelText="Street Address"
+                type="text"
+                name="fromStreetAddress"
+                register={register}
+                errors={errors.fromStreetAddress}
+              />
+              <div className="flex gap-4">
+                <Input
+                  id="billToCity"
+                  labelText="City"
+                  type="text"
+                  name="fromStreetAddress"
+                  register={register}
+                  errors={errors.fromStreetAddress}
+                />
+                <Input
+                  id="billToPostalCode"
+                  labelText="Postal Code"
+                  type="number"
+                  name="fromStreetAddress"
+                  register={register}
+                  errors={errors.fromStreetAddress}
+                />
+                <Input
+                  id="billToCountry"
+                  labelText="Country"
+                  type="text"
+                  name="fromStreetAddress"
+                  register={register}
+                  errors={errors.fromStreetAddress}
+                />
+              </div>
+              <div className="flex gap-4 ">
+                <Input
+                  type="date"
+                  labelText="Invoice Date"
+                  id="billToInvoiceDate"
+                  name="fromStreetAddress"
+                  register={register}
+                  errors={errors.fromStreetAddress}
+                />
+
+                <div className="flex-1 flex flex-col  relative">
+                  <label
+                    htmlFor="payementTerms"
+                    className="opacity-[0.8] mt-4 mb-2 "
                   >
-                    <span className="fa-solid fa-trash"></span>
-                  </button>
+                    Payment Terms
+                  </label>
+                  <select
+                    id="payementTerms"
+                    className="p-[0.6rem] border-[0.1rem] border-[#e5e7eb] rounded-lg bg-transparent focus:outline-[#7C5DFA] appearance-none"
+                  >
+                    <option value="1">Net 1 Day</option>
+                    <option value="7">Net 7 Days</option>
+                    <option value="14">Net 14 Days</option>
+                    <option value="30">Net 30 Days</option>
+                  </select>
+                  <span
+                    className={`fa-solid fa-angle-down transition-all duration-300 absolute top-[60%] right-6`}
+                  ></span>
                 </div>
-              ))}
+              </div>
+              <Input
+                type="text"
+                labelText="Description"
+                id="description"
+                name="fromStreetAddress"
+                register={register}
+                errors={errors.fromStreetAddress}
+              />
+
+              <h3 className="font-bold my-8  opacity-[0.7]">ItemList</h3>
+              <div className="flex flex-col itemList ">
+                {itemList.map((item, index) => (
+                  <div className="flex gap-4 " key={item.id}>
+                    <Input
+                      type="text"
+                      labelText="Item Name"
+                      id="itemName"
+                      name="fromStreetAddress"
+                      register={register}
+                      errors={errors.fromStreetAddress}
+                    />
+                    <Input
+                      type="number"
+                      labelText="Qty."
+                      id="quantity"
+                      name="fromStreetAddress"
+                      register={register}
+                      errors={errors.fromStreetAddress}
+                    />
+                    <Input
+                      type="number"
+                      labelText="Price"
+                      id="price"
+                      name="fromStreetAddress"
+                      register={register}
+                      errors={errors.fromStreetAddress}
+                    />
+                    <div className="flex flex-col ">
+                      <label htmlFor="total" className="opacity-[0.8] mt-4  ">
+                        Total
+                      </label>
+                      <span className=" p-2 mt-3 block w-10">{item.total}</span>
+                    </div>
+                    <button
+                      className="my-4 block hover:text-red-400 self-center"
+                      onClick={() => removeItem(item.id)}
+                    >
+                      <span className="fa-solid fa-trash"></span>
+                    </button>
+                  </div>
+                ))}
+              </div>
+
+              <button
+                onClick={addItem}
+                className="my-8 block w-full lightBtn p-4 rounded-full font-semibold"
+              >
+                + Add New Item
+              </button>
             </div>
+          </div>
 
+          <div className="flex justify-between gap-4 py-4 mt-auto ">
             <button
-              onClick={addItem}
-              className="my-8 block w-full lightBtn p-4 rounded-full font-semibold"
+              className="lightBtn p-3 px-8 rounded-full"
+              onClick={closeModal}
             >
-              + Add New Item
+              Discard
             </button>
+            <div className="flex gap-4">
+              <button className="p-3 bg-[#363B53] ml-8 px-6 text-gray-300 rounded-full">
+                Save as Draft
+              </button>
+              <button className="p-3 bg-[#7C5DFA]  px-6 text-gray-300 rounded-full">
+                Save & Send
+              </button>
+            </div>
           </div>
-        </div>
-
-        <div className="flex justify-between gap-4 py-4 mt-auto ">
-          <button
-            className="lightBtn p-3 px-8 rounded-full"
-            onClick={closeModal}
-          >
-            Discard
-          </button>
-          <div className="flex gap-4">
-            <button className="p-3 bg-[#363B53] ml-8 px-6 text-gray-300 rounded-full">
-              Save as Draft
-            </button>
-            <button className="p-3 bg-[#7C5DFA]  px-6 text-gray-300 rounded-full">
-              Save & Send
-            </button>
-          </div>
-        </div>
+        </form>
       </dialog>
     </>
   );
